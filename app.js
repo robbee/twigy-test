3dd44325855a5d5984473e92a6778e04fe39a716
robbee <alexandre.bonte@gmail.com>
2016-06-11 17:56:45 +0200

    setup

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.get('/webhook', function(req, res) {
 app.get("/test", function(req,res){
 	console.log("test:ok");
 	res.status(200).send('receive : ' + req.query.id);
-	sendTextMessage(100003400399290, req.query.msg);
+	sendTextMessage(req.query.id, req.query.msg);
 	//sendButtonMessage(1300752483272187);
 });
 
